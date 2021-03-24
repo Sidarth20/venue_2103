1cae841bd22b23a5a3130f2a3d55d61cc54cb7ff
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-03-24 15:59:32 -0600

    Attempt at Iteration 2

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,18 +1,23 @@
 class Venue
-  attr_reader :name,
-              :capacity
+  attr_accessor :name,
+                :capacity,
+                :patrons
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
-  end
-
-  def patrons()
-    patrons = []
+    @patrons = []
   end
 
   def add_patron(patrons)
-    patrons.push
-    puts patrons
+    @patrons.push(patrons)
   end
+
+#Couldn't figure out this part of the iteration...
+# Tried different ways to call add_patrons method to capitalize string in spec file
+  def yell_at_patrons(people)
+      yell = add_patron(patrons)
+      yell.upcase(people)
+  end
+
 end
