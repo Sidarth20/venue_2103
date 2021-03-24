d62898cb07f5cb1acd39a1d58fb0db6ffeba3490
Sidarth Bagawandoss <sidarth20@gmail.com>
2021-03-24 15:14:26 -0600

    Complete Iteration 1

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,18 @@
 class Venue
+  attr_reader :name,
+              :capacity
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+  end
+
+  def patrons()
+    patrons = []
+  end
+
+  def add_patron(patrons)
+    patrons.push
+    puts patrons
+  end
 end
